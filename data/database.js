d07c9971467dcab0a858a143ec06260f98075452
shreysavani <shrey124.rejoice@gmail.com>
2024-01-02 21:27:00 -0500

    fixed engines

--- data/database.js
+++ data/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose"
 
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, { dbName: "authDatabase" })
-        .then(() => { console.log('Connected to MongoDB'); })
+        .then((c) => { console.log(`Connected to MongoDB ${c.connection.host}`); })
         .catch(e => console.log(e))
 }
